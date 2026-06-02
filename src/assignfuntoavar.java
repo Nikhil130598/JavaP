635c0178990111645295e944b98b58af14a45a55
chsai <chsainikhil98@gamil.com>
2026-06-02 09:46:46 +0530

    To assign a function to a variable

---
+++ src/assignfuntoavar.java
@@ -0,0 +1,10 @@
+import java.util.function.Function;
+
+public class assignfuntoavar {
+
+        public static void main(String[] args){
+            Function<Integer, Integer> square = x ->x*x;
+            System.out.println(square.apply(5));
+        }
+    }
+
